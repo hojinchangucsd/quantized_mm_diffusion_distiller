a845b273a7683506858dc332d650295a6d07412d
Hojin Chang <hmchang@ucsd.edu>
2024-01-14 01:02:23 -0800

    more images

--- scripts/fid_from_stats.sh
+++ scripts/fid_from_stats.sh
@@ -2,7 +2,8 @@
 cd /home/mmorafah@AD.UCSD.EDU/Progressive_Distillation/diffusion_distiller/
 
 dp=./data/fid_stats/celeba_hq_256_fid-stats.npz
-qp=./data/fid_stats/celeba-u_quant-fid-stats.npz
+sqp=./data/fid_stats/celeba-u_static-quant-fid-stats.npz
+qatp=./data/fid_stats/celeba-u_qat-fid-stats.npz
 fp=./data/fid_stats/celeba-u_float-fid-stats.npz
 
-python -m pytorch_fid --device cuda:1 "$fp" "$qp"
+python -m pytorch_fid --device cuda:1 "$dp" "$sqp"
--- scripts/save_fid_stats.sh
+++ scripts/save_fid_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/mmorafah@AD.UCSD.EDU/Progressive_Distillation/diffusion_distiller
 
-imgs_path=./data/celeba-u_quant-model-out/
-stats_path=./data/fid_stats/celeba-u_quant-fid-stats.npz
+imgs_path=./data/celeba-u_qat-quant-model-out/
+stats_path=./data/fid_stats/celeba-u_qat-fid-stats.npz
 
 python -m pytorch_fid --device cuda:1 --save-stats $imgs_path $stats_path
