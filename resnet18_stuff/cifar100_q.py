5288a15f180330b019f182895da9ddee21caae12
Hojin Chang <hmchang@ucsd.edu>
2024-01-22 14:07:05 -0800

    resnet refactor

--- resnet18_stuff/cifar100_q.py
+++ resnet18_stuff/cifar100_q.py
@@ -6,80 +6,55 @@ sys.path.append('../')
 
 import torch
 from torch.ao.quantization import (
-    get_default_qconfig
+    get_default_qconfig, get_default_qat_qconfig
 )
 from torch.ao.quantization.quantize_fx import (
-    prepare_fx, convert_fx
+    prepare_fx, convert_fx, prepare_qat_fx
 )
 from torch.ao.quantization import QConfigMapping
 from torch.ao.quantization.fx.custom_config import PrepareCustomConfig
 from q_helpers import *
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-from resnet18_stuff.resnet import ResNet, BasicBlock
-from torchvision.datasets import CIFAR100
-from torchvision import transforms
-#from torchvision.models import resnet18, ResNet18_Weights
+from resnet18_wrapper import resnet18_wrapper
+import resnet18_config
 
-transform_train = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
-])
 
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
-])
+QAT = True
+LOAD_CKPT = False
 
-train_ds = CIFAR100('../data', train=True, transform=transform_train, download=True)
-test_ds = CIFAR100('../data', train=False, transform=transform_test, download=True)
 
-train_dl = DataLoader(dataset=train_ds, batch_size=128, shuffle=True, drop_last=False, \
-                     num_workers=0, pin_memory=True)
-test_dl = DataLoader(dataset=test_ds, batch_size=128, shuffle=False, drop_last=False, \
-                     num_workers=0, pin_memory=True)
+resnetw = resnet18_wrapper(resnet18_config.get_quant_config())
 
-num_classes = len(train_ds.classes)
 
-fmodel = ResNet(BasicBlock, [2,2,2,2], num_classes=num_classes)
-pt_weights = torch.load('../checkpoints/CIFAR100/ResNet18_ckpt.pt')
-fmodel.load_state_dict(pt_weights)
-#fmodel = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1,num_classes=num_classes)
+if LOAD_CKPT: 
+    resnetw.load_ckpt()
+    
 device = torch.device("cuda:1")
 backend = "x86"
 
-qconfig = get_default_qconfig(backend)
+qconfig = get_default_qconfig(backend) if not QAT else get_default_qat_qconfig(backend)
 torch.backends.quantized.engine = backend
 qconfig_mapping = QConfigMapping() \
     .set_global(qconfig)
 prepare_fx_custom_map = PrepareCustomConfig()
-example_inputs = (test_ds.__getitem__(0)[0].unsqueeze(dim=0))
+example_inputs = (next(iter(resnetw.train_dl))[0])
 
-fmodel = fmodel.eval().to(device)
+if not QAT: 
+    fmodel = resnetw.model.eval().to(device)
+    pmodel = prepare_fx(fmodel, qconfig_mapping, 
+                        example_inputs, prepare_fx_custom_map)
+    resnetw.model = pmodel
+    resnetw.calibrate(100)
 
-pmodel = prepare_fx(fmodel, qconfig_mapping, 
-                    example_inputs, prepare_fx_custom_map)
+else: 
+    fmodel = resnetw.model.train().to(device)
+    pmodel = prepare_qat_fx(fmodel, qconfig_mapping, example_inputs,
+                            prepare_fx_custom_map)
+    resnetw.model = pmodel
+    resnetw.train_model()
 
-def calibrate(it=100): 
-    with torch.no_grad():
-        for i in tqdm(range(it), desc=f'Calibration'): 
-            pmodel(train_ds.__getitem__(i)[0].to(device).unsqueeze(dim=0))
-
-calibrate()
-
-def get_acc(m): 
-    num_correct = 0
-    with torch.no_grad(): 
-        for inputs, targets in tqdm(test_dl,desc=f'Testing CIFAR100'): 
-            inputs, targets = \
-                inputs.to(device=device,non_blocking=True), \
-                targets.to(device=device,non_blocking=True)
-            out = m(inputs)
-            num_correct += targets.eq(out.argmax(dim=1)).sum()
-    return num_correct / len(test_dl.dataset)
-
-ftest_acc = get_acc(fmodel)
+resnetw.model = fmodel
+ftest_acc = resnetw.get_acc(device)
 print(f'ResNet18 -- Full model CIFAR100 Acc: {ftest_acc:.4f}')
 
 device = "cpu"
@@ -87,5 +62,6 @@ pmodel.eval().to(device)
 qmodel = convert_fx(pmodel)
 qmodel.to(device)
 
-qtest_acc = get_acc(qmodel)
+resnetw.model = qmodel
+qtest_acc = resnetw.get_acc(device)
 print(f'ResNet18 -- Q model CIFAR100 Acc: {qtest_acc:.4f}')
--- resnet18_stuff/nohup.sh
+++ resnet18_stuff/nohup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/mmorafah@AD.UCSD.EDU/Progressive_Distillation/diffusion_distiller
 eval "$(conda shell.bash hook)"
-conda activate FLCP+fb
+conda activate prog_dist
 
-nohup python ./resnet18_stuff/train_resnet18.py > ./resnet18_stuff/nohup_train_resnet18.out &
+nohup python ./resnet18_stuff/cifar100_q.py > ./resnet18_stuff/nohup.out &
